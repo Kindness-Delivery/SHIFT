76aa0255a4afdc0543c575a56ade68803be0b33d
Anonymous <anonymous@mailinator.com>
2023-10-29 18:15:38 +0100

    moved a.pl to a.pm

---
+++ lib/a.pm
@@ -0,0 +1,46 @@
+#!/usr/bin/perl
+# $Intent: act w/ Love and Kindness $
+
+# $Author: michelc $
+# $Created-On: Wed, 2023-10-04 09:24:32 $
+# $Last-Modified: Sun, 2023-10-29 18:12:15 $
+# .-1! echo "\# \$Last-Modified: $(date +'\%a, \%Y-\%m-\%d \%T') \$"
+
+package a;
+use Exporter qw(import);
+@EXPORT = grep { $_ !~ m/^_/ && defined &$_; } keys %{__PACKAGE__ . '::'};
+use strict;
+use lib '/ipfs/QmQjY4TSfTGP11eFfzzz4FJyr8t8P8EaqHZKvdLLuwLNH5';
+use essential qw(version keyw);
+
+use vars qw/$dbug $VERSION/;
+# ----------------------------------------------------
+our $VERSION = sprintf "%d.%02d", q$Revision: 0.0 $ =~ /: (\d+)\.(\d+)/;
+my ($State) = q$State: Exp $ =~ /: (\w+)/; our $dbug = ($State eq 'dbug')?1:0;
+# ----------------------------------------------------
+$VERSION = &version(__FILE__) unless ($VERSION ne '0.00');
+
+printf STDERR "--- # %s: %s %s\n",__PACKAGE__,$VERSION,join', ',caller(0)||caller(1);
+# -----------------------------------------------------------------------
+
+use lib $ENV{HOME}.'/projects/benches/radiant/SHFIT/lib';
+
+$\ = $/ || "\n";
+
+if (__FILE__ eq $0) {
+# -----------------------------------------------------------
+#understand variable=value on the command line...
+eval "\$$1='$2'"while $ARGV[0] =~ /^(\w+)=(.*)/ && shift;
+# -----------------------------------------------------------
+
+printf "--- %s${\}",${0}; # "}"
+## TODO: place your code here
+
+print "...";
+exit $?;
+
+}
+
+# vim: syntax=perl
+# $! echo "1; \# \$Source: /ipfs/$(ipfs add -w % -Q)/%:t \$"
+1; # $Source: /ipfs/QmYj4FdAh5DfXdu7pnHaWjkFHwxTVTX4i5CTyuVVYo3Qkd/a.pm $
--- lib/publish.sh
+++ lib/publish.sh
@@ -1,7 +1,25 @@
 #
-
 tics=$(date +%s)
+SITE=$(readlink -f $(dirname $0)/..)
+echo "--- # $0"
+
 find . -name '*~1' -delete
+ipfs add -n -w -r $SITE/lib $SITE/etc
+qmsite=$(ipfs add -Q -w -r $SITE/lib $SITE/etc)
+echo qmsite: $qmsite
 qm=$(ipfs add -Q -r $PWD)
 echo $tics: $qm >> qm.log
-ipfs add -w *.pm *.pl
+ipfs add -w *.p?
+qmlib=$(ipfs add -n -Q -w *.p?)
+f=a.pm
+perl -pn \
+ -e "s,({?[A-Z][A-Z_]+}?)=(['\"])?/ipfs/\w+(['\"])?,\$1=\2/ipfs/${qmsite}\3,;" \
+ -e "s,-e '/ipfs/\w*',-e '/ipfs/$qmlib',;" \
+ -e "s,use lib '/ipfs/\w*';,use lib '/ipfs/$qmlib';,;" \
+ $f > $f~
+if [ "$?" -eq 0 ]; then
+  if ! diff --color $f $f~; then # ∃ a diff
+    mv $f~ $f
+  fi
+fi
+
